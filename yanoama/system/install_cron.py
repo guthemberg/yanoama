d41ef8314f2b74357bc1ac9be549f2f5783bb9f0
guthemberg <guthemberg.silvestre@lip6.fr>
2012-10-27 18:02:41 +0200

    cron install

---
+++ yanoama/system/install_cron.py
@@ -0,0 +1,17 @@
+import sys
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+try:
+    config_file = file('/etc/yanoama.conf').read()
+    config = json.loads(config_file)
+except Exception, e:
+    print "There was an error in your configuration file (/etc/yanoama.conf)"
+    raise e
+
+
+_backend = config.get('coordinators', {})
+
+print _backend

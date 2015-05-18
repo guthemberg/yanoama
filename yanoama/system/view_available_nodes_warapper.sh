d1686add09891a4a8f5179e38d091d82c89909be
guthemberg <guthemberg.silvestre@gmail.com>
2015-05-18 13:39:15 +0200

    home

--- yanoama/system/view_available_nodes_warapper.sh
+++ yanoama/system/view_available_nodes_warapper.sh
@@ -1,6 +1,6 @@
 NODES_FILE=~/yanoama/nodes.pck
 if [ -e ]; then
-  python ~/yanoama/yanoama/monitoring/show_rtt_nodes.py $NODES_FILE |sort -t, -k1 -nr
+python /home/upmc_aren/yanoama/yanoama/monitoring/show_rtt_nodes.py $NODES_FILE |sort -t, -k1 -nr
 else
   echo "$NODES_FILE does not exist"
 fi

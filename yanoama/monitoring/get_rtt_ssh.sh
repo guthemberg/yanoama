1eff3b8d649a950714077f0c19e1d4cb7ff3da8e
guthemberg <guthemberg.silvestre@gmail.com>
2015-07-28 21:55:18 +0200

    yep

--- yanoama/monitoring/get_rtt_ssh.sh
+++ yanoama/monitoring/get_rtt_ssh.sh
@@ -2,4 +2,4 @@
 #output: rtt in milliseconds OR -1 if failed
 
 #it send FIVE TCP SYN packets to SSH port for testing connectivity and returns the RTT in milliseconds, or 0.0 if it fails
-paping -c 5 -p 22 $1|grep Minimum|cut -d= -f2|cut -d, -f1|sed 's/ms//g'|sed 's/ //g'
+paping --nocolor -c 5 -p 22 $1|grep Minimum|cut -d= -f2|cut -d, -f1|sed 's/ms//g'|sed 's/ //g'

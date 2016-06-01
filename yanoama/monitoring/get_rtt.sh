5ecbe820671f3d608a01e1b430f3f51b41b22224
guto <guto@localhost>
2016-06-01 11:34:30 +0200

    update

--- yanoama/monitoring/get_rtt.sh
+++ yanoama/monitoring/get_rtt.sh
@@ -2,4 +2,4 @@
 #output: rtt in milliseconds OR -1 if failed
 
 #it send FIVE icmp packets for testing connectivity and returns the RTT in milli seconds, or -1 if it fails
-ping_result=`ping -qc 5 $1  2> /dev/null` ; echo $ping_result |tail -n1|cut -d= -f2|cut -d/ -f1|awk -F" " '{ if(length($1)>0) printf "%s", $1; else printf "%s", "-1" }'
+ping_result=`ping -i 0.2 -qc 5 $1  2> /dev/null` ; echo $ping_result |tail -n1|cut -d= -f2|cut -d/ -f1|awk -F" " '{ if(length($1)>0) printf "%s", $1; else printf "%s", "-1" }'
